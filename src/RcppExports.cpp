9676656714baa12f0f6764e19c79b6021a144a68
Wencheng Lau-Medrano <luis.laum@gmail.com>
2022-10-20 15:15:07 +0000

    version 1.5.4

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // engine1
 NumericMatrix engine1(NumericMatrix data, NumericMatrix kernel);
 RcppExport SEXP _imagine_engine1(SEXP dataSEXP, SEXP kernelSEXP) {
